57cd25b16d4a0b30f70a31e08e7111e3dd5506cf
HarshChouhan <chouhanharsh256@email.com>
2023-12-21 23:27:42 +0530

    binary tree inorder traversal

---
+++ binary-tree-inorder-traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def inorderTraversal(self, root):
+        result = []
+        if root:
+            result.extend(self.inorderTraversal(root.left))
+            result.append(root.val)
+            result.extend(self.inorderTraversal(root.right))
+        return result
+    
+#create the binary tree
+root = TreeNode(1)
+
+result = Solution().inorderTraversal(root)
+
+print(result)
