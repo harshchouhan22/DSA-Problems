c2fc04a6b26a140d7b1c10a9c7bb758cb4a84000
HarshChouhan <chouhanharsh256@email.com>
2023-12-22 22:27:14 +0530

    symmetric tree

---
+++ symmetric-tree.py
@@ -0,0 +1,19 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+class Solution(object):
+    def isSymmetric(self, root):
+        def is_mirror(left, right):
+            if not left and not right:
+                return True
+            if not left or not right:
+                return False
+            return (left.val == right.val) and is_mirror(left.left, right.right) and is_mirror(left.right, right.left)
+
+        if not root:
+            return True
+        return is_mirror(root.left, root.right)
+
